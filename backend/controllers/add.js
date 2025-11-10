32d37d378ad7990ccc7c137bda982e244c927e6f
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-11-10 19:53:54 +0530

    updating add.js

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,18 @@
-async function addRepo(){
-    console.log("adding files");
+const fs = require('fs').promises;
+const path = require('path');
+
+async function addRepo(filePath){
+    const repoPath = path.resolve(process.cwd(),"VersionControl");
+    const staggingPath = path.join(repoPath, "stagging");
+
+    try{
+        await fs.mkdir(staggingPath,{recursive:true});
+        const fileName = path.basename(filePath);
+        await fs.copyFile(filePath,path.join(staggingPath,fileName));
+        console.log(`File ${fileName} added to stagging area!`);
+    }catch(err){
+        console.error("Error adding to repository",err);
+    }
 }
 
 module.exports = {addRepo};
