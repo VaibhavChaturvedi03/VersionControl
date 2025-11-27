d69b4de00bdc9e2ff565543cbdaa0023c9364b6e
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-11-27 16:46:13 +0530

    updating main router

--- routes/main.router.js
+++ routes/main.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const userRouter = require('./user.router');
+const repoRouter = require('./repo.router');
 
 const mainRouter = express.Router();
 
 mainRouter.use(userRouter);
+mainRouter.use(repoRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello!");
